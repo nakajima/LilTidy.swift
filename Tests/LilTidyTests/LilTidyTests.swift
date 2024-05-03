4ffc13df814dc0736bdda73af8fbec7b660f4783
Pat Nakajima <patnakajima@gmail.com>
2024-05-03 10:10:59 -0700

    add code test

--- Tests/LilTidyTests/LilTidyTests.swift
+++ Tests/LilTidyTests/LilTidyTests.swift
@@ -11,4 +11,25 @@ final class LilTidyTests: XCTestCase {
 			])
 			XCTAssertEqual("<div>sup</div>\n", cleaned)
     }
+
+	func testCode() throws {
+		let html = """
+		func sup() {
+			return
+		}
+		"""
+
+		let cleaned = try LilTidy.clean(html, options: [
+			"output-html": "yes",
+			"doctype": "omit",
+			"show-body-only": "yes",
+			"vertical-space": "auto"
+		])
+
+		XCTAssertEqual("""
+		func sup() {
+			return
+		}
+		""", cleaned)
+	}
 }
