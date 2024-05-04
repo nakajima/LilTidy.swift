11ae06ba9bde927004381229e73d3a2f2178c821
Pat Nakajima <patnakajima@gmail.com>
2024-05-03 18:15:47 -0700

    ?/

--- Tests/LilTidyTests/LilTidyTests.swift
+++ Tests/LilTidyTests/LilTidyTests.swift
@@ -14,12 +14,17 @@ final class LilTidyTests: XCTestCase {
 
 	func testCode() throws {
 		let html = """
+		<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+		<html>
+		<body>
 		func sup() {
 			return
 		}
+		</body>
 		"""
 
 		let cleaned = try LilTidy.clean(html, options: [
+			"indent": "no",
 			"output-html": "yes",
 			"doctype": "omit",
 			"show-body-only": "yes",
